6a588b75184ec0678cb953a57d9cedfea3a552ae
derickcastillo38 <9derick@gmail.com>
2019-04-11 14:19:53 -0500

    Add background and navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="SalonContainer">
+        <div className="topnav">
+          <a className="active" href="#home">Home</a>
+          <a href="#contact">About</a>
+          <a href="#news">Services</a>
+          <a href="#about">Contacts</a>
+          <a href="#about">Portfolio</a>
+        </div>
       </div>
-    );
+  );
   }
 }
 
