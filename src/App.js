43cf5aebb3899366ac354cfe6dabc3ff474f477e
derickcastillo38 <9derick@gmail.com>
2019-04-11 15:11:05 -0500

    Center navbar and booking button, remove name on homepage

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ class App extends Component {
   render() {
     return (
       <div className="SalonContainer">
-        <div className="topnav">
+        <div className="topnav" id="navbar">
           <a className="active" href="#home">Home</a>
           <a href="#contact">About</a>
           <a href="#news">Services</a>
           <a href="#about">Contacts</a>
           <a href="#about">Portfolio</a>
         </div>
-        <h4 className='center' id='RoseSomething'>Rose Pudzis</h4>
         <a
           href="https://www.yelp.com/biz_photos/rowi-salon-chicago?select=zKP3-_9WBefPePMkKRtzgw"
           target="_blank"
